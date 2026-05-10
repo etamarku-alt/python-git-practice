fe729c46f2b5d57d6c65c73fe322b2291a49ca66
itamar <etamarku@gmail.com>
2026-05-10 14:59:01 +0300

    forth commit

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-a=100/0.85
-print(round (a, 4))
-print("hello world", end=" ")
-print("shalom")
+int=float(344545)
+print(int)
+a="ariel"
+b="bney brak"
+c="chaifa"
+d="yakir"
+print(a,b,c,d, sep="-")
