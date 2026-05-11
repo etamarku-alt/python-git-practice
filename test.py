38f96cfb56a2bc61e446225bbb31f346c1c8450f
itamar <etamarku@gmail.com>
2026-05-11 11:33:41 +0300

    the five commit

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
-int=float(344545)
-print(int)
-a="ariel"
-b="bney brak"
-c="chaifa"
-d="yakir"
-print(a,b,c,d, sep="-")
+a=str(input('are you "Tel aviv" resident (yes/no):'))
+if a=="yes":
+    b=int(input("what your age: "))
+    if b>65:
+        print("זכאי להנחה")
+    else:
+        print("אתה צעיר מידי להנחה")
+if a=="no":
+    print("the count is only for resident")
+else:
+    print("not valid informaition")
